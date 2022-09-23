ec6eed5c2bcebd4e8340c5da3a9924e3e1d8b0ac
RCOak <coliveirarenato90@gmail.com>
2022-09-23 19:04:07 +0000

    att

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+ *
     {
         includeCover: true, // if includeCover is true the algorithm should
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
@@ -20,8 +20,8 @@ import "../style/index.css";
         role: null,
         country: null,
         city: null
-    }
- */
+    }*/
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
@@ -33,7 +33,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name}Lucy Boilett</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
