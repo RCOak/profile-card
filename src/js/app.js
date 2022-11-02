1485e4ffe90047e6a23512d4e37376964a28a5e2
RCOak <coliveirarenato90@gmail.com>
2022-11-02 20:27:48 +0000

    att

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,12 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name == null ? "Name" : variables.name}</h1>
+          <h1>${variables.name == null ? "Name" : variables.name} ${
+    variables.lastname == null ? "Last Name" : variables.lastname
+  }</h1>
           <h2>${variables.role == null ? "Role" : variables.role}</h2>
           <h3>${variables.city == null ? "City" : variables.city}</h3>
+          <h3>${variables.country == null ? "Country" : variables.country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
